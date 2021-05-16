0941ee9819fb6f532d6318c78e93e5f24d72871e
Luis Timaná <60704967+luistimana@users.noreply.github.com>
2021-05-16 18:50:34 -0500

    Insertar a Base de Datos Postman
    Insertar a Base de Datos Postman

--- controller/categoria.php
+++ controller/categoria.php
@@ -1,16 +1,28 @@
 <?php
+    header('Content-Type: application/json');
 
     require_once("../config/conexion.php");
     require_once("../models/Categoria.php");
     $categoria = new Categoria();
 
-    switch(isset($_GET["op"])){
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    switch($_GET["op"]){
 
         case "GetAll":
             $datos=$categoria->get_categoria();
             echo json_encode($datos);
         break;
 
+        case "GetId":
+            $datos=$categoria->get_categoria_x_id($body["cat_id"]);
+            echo json_encode($datos);
+        break;
+
+        case "Insert":
+            $datos=$categoria->insert_categoria($body["cat_nom"],$body["cat_obs"]);
+            echo json_encode("Correcto");
+        break;
        
     }
 ?>
--- models/Categoria.php
+++ models/Categoria.php
@@ -8,5 +8,26 @@
             $sql->execute();
             return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
+
+        public function get_categoria_x_id($cat_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM tm_categoria WHERE est=1 AND cat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_categoria($cat_nom,$cat_obs){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO tm_categoria(cat_id,cat_nom,cat_obs,est) VALUES (NULL,?,?,'1');";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_nom);
+            $sql->bindValue(2, $cat_obs);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
 ?>
